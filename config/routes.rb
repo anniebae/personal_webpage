62a1323e4b914e7576b4ad42cfb9d62c44e0eecd
anniebae <bae.annie@gmail.com>
2014-12-23 21:30:15 -0500

    adding skills to index page. not sure what to do yet

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "main#index"
 
-  get '/about' => "main#about"
+
   get '/projects' => "main#projects"
   get '/resume' => "main#resume"
   get '/photography' => "main#photography"
