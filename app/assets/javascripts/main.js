8cd01d50b2b53d89d0730019fbc7a807a823a27a
anniebae <bae.annie@gmail.com>
2014-12-23 21:01:09 -0500

    anniebae logo animates on mouseenter and mouseleave

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,4 +1,19 @@
 $(document).ready(function(){
+
+  $('.anniebae').mouseenter(function(){
+    $(this).animate({
+      height: "90px",
+      width: "90px"
+    });
+  });
+  $('.anniebae').mouseleave(function(){
+    $(this).animate({
+      height: "80px",
+      width: "80px"
+    });
+  });
+
+
   $('.menu-item').hover( 
     function() { $(this).data('bounce', true); bounce($(this));}, 
     function() { $(this).data('bounce', false);
