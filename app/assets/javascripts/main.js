5d217588fd5bf6ede8a9431c5a546adc90ae1fdb
anniebae <bae.annie@gmail.com>
2015-01-13 18:22:59 -0500

    fixed resume formatting

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -20,26 +20,4 @@ $(document).ready(function(){
     $('.project-caption', this).stop(true, true).hide();
   });
 
-
-  // $('.menu-item').hover(function() {
-  //   $(this).data('bounce', true);
-  //   bounce($(this));
-  // }, function() {
-  //   $(this).data('bounce', false);
-  // });
-  
-  // function bounce($elem) {
-  //   $elem.effect('bounce', {
-  //     times: 1,
-  //     distance: 10
-  //   }, 500, function() {
-  //     if ($(this).data('bounce'))
-  //       bounce($elem);
-  //     else
-  //       $elem.stop();
-  //   });
-  // }
-
-
-
 })
